1bd418f196b21d5041570d33e6cd9c77480853f5
danielcrubens <dcr.tecnologi@gmail.com>
2024-12-20 20:46:40 -0300

    api

--- server/api/send.ts
+++ server/api/send.ts
@@ -1,7 +1,7 @@
 // server/api/send.ts
 import { useCompiler } from '#vue-email'
 import { Resend } from 'resend'
-
+ 
 // Inicializa o Resend com a chave API do ambiente
 const resend = new Resend(process.env.RESEND_API_KEY)
 
