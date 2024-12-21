dec0df15de68ef670b5831886e0bd494fb93d2dc
danielcrubens <dcr.tecnologi@gmail.com>
2024-12-21 11:59:28 -0300

    api

--- server/api/send.ts
+++ server/api/send.ts
@@ -10,7 +10,7 @@ export default defineEventHandler(async (event) => {
     // Recebe os dados do corpo da requisição
     const { email, name } = await readBody(event)
 
-    // Compila o template Vue com as props necessáriasssdsghf
+    // Compila o template Vue com as props necessáriasssd
     const template = await useCompiler('Welcome.vue', {
       props: {
         name,
